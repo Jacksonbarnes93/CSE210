50e4f7ae97310c45b3c122e8aa971053fdfed29a
Jackson Barnes <Jacksonbarnes246@gmail.com>
2025-04-23 19:49:22 -0600

    Added if else statements to see if you pass classes

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What grade did you get? ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int grade))
+        {
+            if (grade >= 90)
+            {
+                Console.WriteLine("You got an A!");
+            }
+            else if (grade >= 80)
+            {
+                Console.WriteLine("You got a B!");
+            }
+            else if (grade >= 70)
+            {
+                Console.WriteLine("You got a C!");
+            }
+            else if (grade >= 60)
+            {
+                Console.WriteLine("You got a D!");
+            }
+            else
+            {
+                Console.WriteLine("You got an F!");
+            }
+           if (grade >= 70)
+            {
+                Console.WriteLine("You passed the class! ");
+            }
+            else
+            {
+                Console.WriteLine("You did not pass the class. ");
+            }
+        }
     }
 }
