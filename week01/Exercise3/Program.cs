68c1fccc326507acadefeef98a5dbeb9bd217761
Jackson Barnes <Jacksonbarnes246@gmail.com>
2025-04-24 16:26:15 -0600

    Completed the work for this assignment

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int numberToGuess = random.Next(1,101); //This will generate a random number between 1 and 100
+        int userGuess = 0; //this will store the user's guess   
+
+        while (userGuess != numberToGuess)
+        {
+            Console.WriteLine("Guess a number between 1 and 100:");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out userGuess))
+            {
+
+                if (userGuess < numberToGuess)
+                {
+                    Console.WriteLine("Too low! Try again.");
+                }
+                else if (userGuess > numberToGuess)
+                {
+                    Console.WriteLine("Too high! Try again.");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You've guessed the number!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number between 1 and 100.");
+            }
+        }
     }
 }
